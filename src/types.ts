8d1f7b2cbe5c05107b71681f4ed97b658dc47c83
libondev <bon.li@outlook.com>
2023-07-01 22:38:15 +0800

    feat(type): 为插件添加扩展属性支持

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,17 @@
-export type LifecycleFn = (
+export type LifecycleFn<Context> = (
+  this: Context,
   ctx: CanvasRenderingContext2D,
   opt: Pick<Options, 'viewport' | 'canvas'>
 ) => void
 
-export type Plugin = (config: UserOptions) => ({
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Plugin<Params = Record<string, any>> = (config: UserOptions) => ({
   enforce?: 'pre' | 'post'
-  init?: LifecycleFn
-  draw?: LifecycleFn
-  render?: LifecycleFn
-  destroy?: LifecycleFn
-})
+  init?: LifecycleFn<Params>
+  draw?: LifecycleFn<Params>
+  update?: LifecycleFn<Params>
+  destroy?: LifecycleFn<Params>
+} & Params)
 
 export interface CanvasConfig {
   className?: string

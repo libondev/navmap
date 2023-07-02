240d146a00bfdc1a10649354b9850269825377e7
libondev <bon.li@outlook.com>
2023-07-02 20:20:25 +0800

    perf: optimize code

--- src/plugins/preset.ts
+++ src/plugins/preset.ts
@@ -5,9 +5,10 @@ type PluginContext = {
     recordY: number
     scaleRatio: number
     scrollHeight: number
-    scrollTo: number
-    positions: Array<{ offsetHeight: number, offsetTop: number }>
+    scrollTop: number
+    deltaY: number
   }
+  cachedPositions: Array<{ offsetHeight: number, offsetTop: number }>
 } & Record<
   'onPointerDown' | 'onPointerMove' | 'onPointerUp',
   (this: PluginContext, ev: PointerEvent) => void
@@ -36,34 +37,31 @@ export const PresetPlugin: Plugin<PluginContext> = () => ({
   update (_, { viewport }) {
     const headings = Array.from(viewport.querySelectorAll<HTMLElement>('h1, h2, h3, h4, h5, h6'))
 
-    this.states.positions = headings.map(({ offsetHeight, offsetTop }) => ({ offsetHeight, offsetTop }))
+    this.cachedPositions = headings.map(({ offsetHeight, offsetTop }) => ({ offsetHeight, offsetTop }))
   },
 
   draw (ctx, { canvas, canvas: { width, height }, viewport }) {
-    const { states } = this
+    const { states, states: { scrollTop }, cachedPositions } = this
     const scaleRatio = canvas.offsetHeight / viewport.offsetHeight
-    const scrollTop = visualViewport.pageTop
 
     Object.assign(states, {
       scaleRatio,
       scrollHeight: height * scaleRatio
     })
 
+    ctx.fillStyle = '#00000030'
     ctx.beginPath()
 
-    ctx.fillStyle = '#00000030'
     // 绘制标题块
-    states.positions.forEach(({ offsetHeight, offsetTop }) => {
+    cachedPositions.forEach(({ offsetHeight, offsetTop }) => {
       // 标题块的位置是固定的, 但是高度是缩放的, 所以需要乘以缩放比例
       ctx.rect(0, offsetTop * scaleRatio, width, offsetHeight * scaleRatio)
     })
     ctx.closePath()
-
     ctx.fill()
 
     // 绘制视口
     ctx.fillStyle = '#000'
-
     ctx.strokeRect(0, scrollTop * scaleRatio, width, height * scaleRatio)
   },
 
@@ -73,38 +71,46 @@ export const PresetPlugin: Plugin<PluginContext> = () => ({
     canvas.addEventListener('pointerdown', this.onPointerDown)
   },
 
+  cachedPositions: [],
+
   states: {
     recordY: 0, // 记录鼠标按下时的位置
+    deltaY: 0, // 鼠标按下的位置距离滚动条顶部的位置
     scaleRatio: 0, // 缩放比例
     scrollHeight: 0, // 滚动条高度
-    scrollTo: document.documentElement.scrollTop, // 上一次滚动的位置
-    positions: [] // 标题块的位置
+    get scrollTop () {
+      return visualViewport.pageTop
+    } // 上一次滚动的位置
   },
 
   onPointerDown (ev) {
     ev.preventDefault()
-    const { states, states: { scrollTo, scrollHeight, scaleRatio } } = this
+    const { states, states: { scrollTop, scrollHeight, scaleRatio } } = this
 
     const y = ev.clientY
 
     states.recordY = y
 
-    // 只有按下的位置不在滚动条上才会触发滚动
-    // if (y < scrollTo * scaleRatio && y > scrollTo * scaleRatio + scrollHeight) {
-    this.states.scrollTo = document.documentElement.scrollTop = (y - scrollHeight / 2) / scaleRatio
-    // }
+    // 点击非滚动条的位置时, 将视口滚动到点击位置
+    if (y <= scrollTop * scaleRatio || y >= scrollTop * scaleRatio + scrollHeight) {
+      states.deltaY = y - scrollHeight / 2
+      document.documentElement.scrollTop = states.deltaY / scaleRatio
+    } else {
+      // 点击滚动条时, 重新计算按下的位置与滚动条顶部的距离
+      // states.deltaY = y - scrollTop + scrollHeight / 2
+
+      console.log(states.deltaY)
+    }
 
     document.addEventListener('pointermove', this.onPointerMove)
     document.addEventListener('pointerup', this.onPointerUp, { once: true })
   },
 
   onPointerMove ({ clientY }) {
-    const { states } = this
+    const { recordY, scaleRatio, deltaY } = this.states
 
-    const targetScrollTop = states.scrollTo + (clientY - states.recordY) / states.scaleRatio
-
-    // 将在滚动条上的鼠标位置转换为视口上的位置
-    document.documentElement.scrollTop = targetScrollTop
+    // 将在滚动条上的位置转换为视口上的位置
+    document.documentElement.scrollTop = (clientY - recordY + deltaY) / scaleRatio
   },
 
   onPointerUp () {

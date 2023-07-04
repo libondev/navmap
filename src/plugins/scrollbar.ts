81ac78a49c1f0fca64bb659d1b158121af6a1dfc
libondev <bon.li@outlook.com>
2023-07-04 22:25:53 +0800

    fix: 修复按下滚动条滚动位置会出现偏差的问题

--- src/plugins/scrollbar.ts
+++ src/plugins/scrollbar.ts
@@ -1,12 +1,9 @@
 import type { Plugin } from '../types'
 
 type PluginContext = {
-  data: {
-    recordY: number
-    deltaY: number
-  }
+  deltaY: number
 } & Record<
-  'onPointerDown' | 'onPointerMove' | 'onPointerUp',
+  '_pointerDown' | '_pointerMove' | '_pointerUp',
   (this: PluginContext, ev: PointerEvent) => void
 >
 
@@ -23,64 +20,54 @@ export const Scrollbar: Plugin<PluginContext> = (states) => ({
     viewport.classList.add('hide-scrollbar')
     viewport.appendChild(canvas)
 
-    this.onPointerDown = this.onPointerDown.bind(this)
-    this.onPointerMove = this.onPointerMove.bind(this)
-    this.onPointerUp = this.onPointerUp.bind(this)
+    this._pointerDown = this._pointerDown.bind(this)
+    this._pointerMove = this._pointerMove.bind(this)
+    this._pointerUp = this._pointerUp.bind(this)
 
-    canvas.addEventListener('pointerdown', this.onPointerDown)
+    canvas.addEventListener('pointerdown', this._pointerDown)
   },
 
   draw (ctx, { canvas: { width, height } }) {
     // 绘制视口
     ctx.strokeStyle = '#f00'
 
-    console.log([states.scrollTop, states.scaleRatio, states.scrollHeight])
     ctx.strokeRect(0, states.scrollTop * states.scaleRatio, width, height * states.scaleRatio)
   },
 
   destroy (_, { canvas, viewport }) {
     viewport.classList.remove('hide-scrollbar')
     viewport.removeChild(canvas)
-    canvas.addEventListener('pointerdown', this.onPointerDown)
+    canvas.addEventListener('pointerdown', this._pointerDown)
   },
 
-  data: {
-    deltaY: 0, // 鼠标按下的位置距离滚动条顶部的位置
-    recordY: 0 // 记录鼠标按下时的位置
-  },
+  deltaY: 0,
 
-  onPointerDown (ev) {
+  _pointerDown (ev) {
     ev.preventDefault()
     const { scrollTop, scrollHeight, scaleRatio } = states
-    const { data } = this
 
     const y = ev.clientY
 
-    data.recordY = y
-
-    // 点击非滚动条的位置时, 将视口滚动到点击位置
-    if (y <= scrollTop * scaleRatio || y >= scrollTop * scaleRatio + scrollHeight) {
-      data.deltaY = y - scrollHeight / 2
-      document.documentElement.scrollTop = data.deltaY / scaleRatio
+    // 判断用户是否点击了滚动条
+    if (y >= scrollTop * scaleRatio && y <= (scrollTop * scaleRatio + scrollHeight)) {
+      // 如果用户点击了滚动条，那么我们只需要记录偏移量，不需要滚动
+      this.deltaY = y - (scrollTop * scaleRatio)
     } else {
-      // 点击滚动条时, 重新计算按下的位置与滚动条顶部的距离
-      // data.deltaY = y - scrollTop + scrollHeight / 2
-
-      // console.log(data.deltaY)
+      // 如果用户点击了滚动条以外的位置，那么我们需要立即滚动到目标位置，并且让滚动条在点击位置的中心
+      this.deltaY = scrollHeight / 2
+      document.documentElement.scrollTop = (y - this.deltaY) / scaleRatio
     }
 
-    document.addEventListener('pointermove', this.onPointerMove)
-    document.addEventListener('pointerup', this.onPointerUp, { once: true })
+    document.addEventListener('pointermove', this._pointerMove)
+    document.addEventListener('pointerup', this._pointerUp, { once: true })
   },
 
-  onPointerMove ({ clientY }) {
-    const { recordY, deltaY } = this.data
-
+  _pointerMove ({ clientY }) {
     // 将在滚动条上的位置转换为视口上的位置
-    document.documentElement.scrollTop = (clientY - recordY + deltaY) / states.scaleRatio
+    document.documentElement.scrollTop = (clientY - this.deltaY) / states.scaleRatio
   },
 
-  onPointerUp () {
-    document.removeEventListener('pointermove', this.onPointerMove)
+  _pointerUp () {
+    document.removeEventListener('pointermove', this._pointerMove)
   }
 })
